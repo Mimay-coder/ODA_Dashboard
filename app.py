edbcac540f341dc4e130e7c86fd61112a8ae388b
Mimay-coder <mayyanike@gmail.com>
2025-05-26 00:07:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -235,6 +235,9 @@ elif section == "Education Indicators":
     col_male, col_female = st.columns((5,5))
     with col_male:
         st.markdown("<h5 style='margin-bottom: -1.4rem;'>Out of School -Male </h5>", unsafe_allow_html=True)
+
+        st.write("school_df columns:", school_df.columns)
+        st.write(school_df.head())
         school_df = Finaldf1[(Finaldf1['Country'] == country) &(Finaldf1['Year'].between(2000, 2019))]
         school_df = school_df.dropna(subset=['Out_of_Schcool_Male'])
         
