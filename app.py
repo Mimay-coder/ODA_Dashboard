35f34d62d3ac04e595cc920a7f7357c5dd75ccf0
Mimay-coder <mayyanike@gmail.com>
2025-05-25 21:03:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -163,16 +163,20 @@ elif section == "Healthcare Indicators":
         fig_Nourishment.add_scatter(x=Nourishment_data['Year'], y=Nourishment_data['Undernourishment'],name='Population Undernourished (%)',
         yaxis='y2')
         
-        fig_Nourishment.update_layout(height=270,margin=dict(t=0, b=10, l=0, r=0),font=dict(size=12, family='Arial Black'),
-        legend=dict(orientation="h",y=-0.3,font=dict(size=12, family='Arial Black')),
+       fig_Nourishment.update_layout(height=270,margin=dict(t=0, b=10, l=0, r=0),font=dict(size=12, family='Arial Black'),
+       legend=dict(orientation="h",y=-0.3,font=dict(size=12, family='Arial Black')
+        ),
+
+        yaxis=dict(title='Basic Nutrition ODA (Millions)',range=[0, Nourishment_data['Sector_ODA_Millions'].max() * 1.2],
+        titlefont=dict(size=15, family='Arial Black'),tickfont=dict(size=12, family='Arial Black')
+        ),
         
-                                      yaxis=dict(title='Basic Nutrition ODA (Millions)',
-        range=[0, Nourishment_data['Sector_ODA_Millions'].max() * 1.2],titlefont=dict(size=15, family='Arial Black'),
-        tickfont=dict(size=12, family='Arial Black')), yaxis2=dict(title='Undernourishment (%)',overlaying='y',side='right',
-        range=[0, Nourishment_data['Undernourishment'].max() * 3],titlefont=dict(size=15, family='Arial Black'),
-        tickfont=dict(size=12, family='Arial Black')),
-                                      
-        xaxis=dict(title='Year',titlefont=dict(size=15, family='Arial Black'),tickfont=dict(size=12, family='Arial Black')))
+        yaxis2=dict(title='Undernourishment (%)',overlaying='y',side='right',range=[0, Nourishment_data['Undernourishment'].max() * 3],
+        titlefont=dict(size=15, family='Arial Black'),tickfont=dict(size=12, family='Arial Black')
+        ),
+        xaxis=dict(title='Year',titlefont=dict(size=15, family='Arial Black'),tickfont=dict(size=12, family='Arial Black')
+    )
+)
         st.plotly_chart(fig_Nourishment, use_container_width=True)
         st.markdown('</div>', unsafe_allow_html=True)
 
