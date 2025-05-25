62b2ceb1cdda7f7f4c7712b9aedfae3c44ffee7f
Mimay-coder <mayyanike@gmail.com>
2025-05-25 11:37:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ if section == "AID Landscape":
          st.markdown("<div style='margin-top: -5rem; margin-bottom: 0.5rem;'>Top Donors</div>", unsafe_allow_html=True)
          donor_data = map_data.groupby('Donor')['Sector_ODA_Millions'].sum().nlargest(10).reset_index()
          fig_donor = px.bar(donor_data, x='Donor', y='Sector_ODA_Millions', title=f"Top 10 Donors in {year}")
-         fig_donor.update_layout(height=400, margin=dict(t=5, b=5, l=5, r=5))
+         fig_donor.update_layout(height=200, margin=dict(t=5, b=5, l=5, r=5))
          st.plotly_chart(fig_donor, use_container_width=True)
 
 
