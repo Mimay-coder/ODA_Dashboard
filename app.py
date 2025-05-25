5bcd25e2666e74146b475b26c47e5ddd34a90fe4
Mimay-coder <mayyanike@gmail.com>
2025-05-25 17:29:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -138,9 +138,9 @@ elif section == "Healthcare Indicators":
         st.plotly_chart(fig_malaria, use_container_width=True)
         st.markdown('</div>', unsafe_allow_html=True)
 
-        col_san = st.columns([1, 6, 1])
+        col_san = st.columns([1,0])
     with col_san[1]:
-        st.plotly_chart(fig_sanitation, use_container_width=True)
+        
         sanitation_data = Finaldf[(Finaldf['Country'] == country) &(Finaldf['Sector'] == 'Water supply & sanitation')].groupby('Year').agg({
         'Sector_ODA_Millions':'sum','Population_using_basic_sanitation%': 'mean'}).reset_index()
         
