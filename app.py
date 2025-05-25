323fc9eb52924a49a3ece6d01e685b66254b8e60
Mimay-coder <mayyanike@gmail.com>
2025-05-25 12:21:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if section == "AID Landscape":
 
     map_data = Finaldf[(Finaldf['Year'] == year) & (Finaldf['Sector'] == 'All sectors')]
 
-    col_map, col_pie = st.columns((5.5, 4.5))
+    col_map, col_pie = st.columns((5,5))
     with col_map:
         st.markdown("ODA per Capita by Country")
         fig_map = px.choropleth(
