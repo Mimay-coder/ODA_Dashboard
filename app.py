b24e6c5369a60be151a71311b1c9e8e8b87d3cb2
Mimay-coder <mayyanike@gmail.com>
2025-05-25 11:42:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ if section == "AID Landscape":
         sector_sum = sector_data.groupby('Sector')['Sector_ODA_Millions'].sum()
         fig_pie = px.pie(sector_sum, values=sector_sum.values, names=sector_sum.index,
                          color_discrete_sequence=px.colors.sequential.Blues)
-        fig_pie.update_layout(height=400, margin=dict(t=5, b=20, l=5, r=5), legend=dict(orientation="v", y=0.5))
+        fig_pie.update_layout(height=400, margin=dict(t=5, b=5, l=5, r=5), legend=dict(orientation="v", y=0.5))
         st.plotly_chart(fig_pie, use_container_width=True)
 
     
