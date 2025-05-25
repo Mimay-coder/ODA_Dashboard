3a6ee751b758c1bae824ee7e540cb27652523459
Mimay-coder <mayyanike@gmail.com>
2025-05-25 21:22:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -163,7 +163,10 @@ elif section == "Healthcare Indicators":
         fig_Nourishment.add_scatter(x=Nourishment_data['Year'], y=Nourishment_data['Undernourishment'],name='Population Undernourished(%)',
         yaxis='y2')
         fig_Nourishment.update_layout(height=270, margin=dict(t=0, b=10, l=0, r=0),legend=dict(orientation="h", y=-0.3),
-        yaxis2=dict(title='Undernourishment',overlaying='y',side='right'))
+        yaxis2=dict(title='Undernourishment',overlaying='y',side='right',range=[0, Nourishment_data['Undernourishment'].max() * 1.5]))
+        
+        yaxis=dict(title='Basic Nutrition ODA (Millions)',range=[0, Nourishment_data['Sector_ODA_Millions'].max() * 1.2])
+
 
         st.plotly_chart(fig_Nourishment, use_container_width=True)
         st.markdown('</div>', unsafe_allow_html=True)
