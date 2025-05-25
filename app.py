53e8f704578eda807d197062993534c83e345217
Mimay-coder <mayyanike@gmail.com>
2025-05-25 16:37:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ if section == "AID Landscape":
          st.plotly_chart(fig_donor, use_container_width=True)
     
     with col_channel:
-        st.markdown("<h5 style='margin-bottom: -0.5rem;'>ODA by Channel</h5>", unsafe_allow_html=True)
+        st.markdown("<h5 style='margin-bottom: -2.1rem;'>ODA by Channel</h5>", unsafe_allow_html=True)
         # Filter data for the selected year and non-null channel
         sector_filtered2 = Finaldf[
         (Finaldf['Year'] == year) &
@@ -91,10 +91,10 @@ if section == "AID Landscape":
             sector_sum = sector_filtered2.groupby('Channel')['Sector_ODA_Millions'].sum().reset_index()
             
             fig_channel = px.pie(sector_sum,names='Channel',values='Sector_ODA_Millions',hole=0.3,
-            title=f'ODA by Channel - {year}',color_discrete_sequence=["#08306B", "#08519C", "#2171B5", "#4292C6", "#6BAED6",
+                                 color_discrete_sequence=["#08306B", "#08519C", "#2171B5", "#4292C6", "#6BAED6",
                                                                               "#9ECAE1", "#C6DBEF", "#DEEBF7"])
             fig_channel.update_traces(texttemplate='%{percent:.1%}')
-            fig_channel.update_layout(height=250,margin=dict(t=10, b=10, l=10, r=10),legend=dict(orientation="h", y=-0.3))
+            fig_channel.update_layout(height=250,margin=dict(t=0, b=10, l=0, r=10),legend=dict(orientation="h", y=-0.3))
             st.plotly_chart(fig_channel, use_container_width=True)
             st.markdown('</div>', unsafe_allow_html=True)
 
