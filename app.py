c04db0e81e2e1f12368650a4645955abfb4d289e
Mimay-coder <mayyanike@gmail.com>
2025-05-25 21:00:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,9 @@ elif section == "Healthcare Indicators":
         yaxis='y2')
         
         fig_Nourishment.update_layout(height=270,margin=dict(t=0, b=10, l=0, r=0),font=dict(size=12, family='Arial Black'),
-        legend=dict(orientation="h",y=-0.3,font=dict(size=12, family='Arial Black')),yaxis=dict(title='Basic Nutrition ODA (Millions)',
+        legend=dict(orientation="h",y=-0.3,font=dict(size=12, family='Arial Black')),
+        
+                                      yaxis=dict(title='Basic Nutrition ODA (Millions)',
         range=[0, Nourishment_data['Sector_ODA_Millions'].max() * 1.2],titlefont=dict(size=15, family='Arial Black'),
         tickfont=dict(size=12, family='Arial Black')), yaxis2=dict(title='Undernourishment (%)',overlaying='y',side='right',
         range=[0, Nourishment_data['Undernourishment'].max() * 3],titlefont=dict(size=15, family='Arial Black'),
