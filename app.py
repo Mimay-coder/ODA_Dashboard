dc5d28c0b9c8de37f7a44bdd761b15995621cf11
Mimay-coder <mayyanike@gmail.com>
2025-05-25 16:43:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -70,12 +70,12 @@ if section == "AID Landscape":
         fig_pie.update_layout(height=200, margin=dict(t=0, b=0, l=4, r=6), legend=dict(orientation="v", y=0.5))
         st.plotly_chart(fig_pie, use_container_width=True)
 
-    col_donor, col_channel = st.columns((5,5))
+    col_donor, col_channel = st.columns((6,5))
     with col_donor:
          st.markdown("<h5 style='margin-bottom: -2.1rem;'>Top Donors</h5>", unsafe_allow_html=True)
          donor_data = map_data.groupby('Donor')['Sector_ODA_Millions'].sum().nlargest(10).reset_index()
          fig_donor = px.bar(donor_data, x='Donor', y='Sector_ODA_Millions')
-         fig_donor.update_layout(height=220, margin=dict(t=0, b=0, l=3, r=3))
+         fig_donor.update_layout(height=250, margin=dict(t=0, b=0, l=3, r=3))
          st.plotly_chart(fig_donor, use_container_width=True)
     
     with col_channel:
@@ -89,12 +89,11 @@ if section == "AID Landscape":
             st.warning(f"No data for year {year}")
         else:
             sector_sum = sector_filtered2.groupby('Channel')['Sector_ODA_Millions'].sum().reset_index()
-            
             fig_channel = px.pie(sector_sum,names='Channel',values='Sector_ODA_Millions',hole=0.3,
                                  color_discrete_sequence=["#08306B", "#08519C", "#2171B5", "#4292C6", "#6BAED6",
                                                                               "#9ECAE1", "#C6DBEF", "#DEEBF7"])
             fig_channel.update_traces(texttemplate='%{percent:.1%}')
-            fig_channel.update_layout(height=250,margin=dict(t=0, b=10, l=0, r=10),legend=dict(orientation="v", y=0.5))
+            fig_channel.update_layout(height=250,margin=dict(t=0, b=5, l=0, r=0),legend=dict(orientation="v", y=0.5))
             st.plotly_chart(fig_channel, use_container_width=True)
             st.markdown('</div>', unsafe_allow_html=True)
 
