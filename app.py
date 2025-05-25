8c3e592f5e09432751a1ffe01487eb2263325977
Mimay-coder <mayyanike@gmail.com>
2025-05-25 17:30:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ elif section == "Healthcare Indicators":
         st.markdown('</div>', unsafe_allow_html=True)
 
         col_san = st.columns([1,0])
-    with col_san[1]:
+    with col_san:
         
         sanitation_data = Finaldf[(Finaldf['Country'] == country) &(Finaldf['Sector'] == 'Water supply & sanitation')].groupby('Year').agg({
         'Sector_ODA_Millions':'sum','Population_using_basic_sanitation%': 'mean'}).reset_index()
